c0a5cc940ed8b36ff86c78a1276527c65a88eaaf
YOUR NAME <andrew_yudaichev@i.ua>
2016-09-22 18:37:22 +0300

    zzz
    1

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
         //new changes
         
-        tttt
+        /*tttt*/
     }
 }
---
+++ src/x.java
@@ -0,0 +1,5 @@
+/**
+ * Created by an.yudaichev on 22.09.2016.
+ */
+public class x {
+}
